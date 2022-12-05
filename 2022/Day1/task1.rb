99fa7922388d86f751171cf0140bca078a4dd3d5
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-05 18:19:09 +0200

    File.readlines

--- 2022/Day1/task1.rb
+++ 2022/Day1/task1.rb
@@ -1,15 +1,1 @@
-max_calories = 0
-current_deer = 0
-
-IO.foreach("input1.txt") do |x| 
-  x.chomp!
-
-  if x.empty?
-    max_calories = current_deer if current_deer > max_calories
-    current_deer = 0
-  else
-    current_deer += x.to_i
-  end
-end
-
-puts max_calories
+p File.readlines("input1.txt").map(&:to_i).slice_after(&:zero?).map(&:sum).max(1).sum
--- 2022/Day3/tasks.rb
+++ 2022/Day3/tasks.rb
@@ -1,10 +1,10 @@
-p File.readlines(ARGV[0]).sum {
+p File.readlines(ARGV[0]).sum do
   l, r = _1.chars.each_slice(_1.length / 2).to_a
   c = (l & r)[0].ord
   c - (c < 91 ? 38 : 96)
-}
+end
 
-p File.readlines(ARGV[0]).map(&:chars).each_slice(3).sum {
+p File.readlines(ARGV[0]).map(&:chars).each_slice(3).sum do
   c = _1.reduce(:&)[0].ord
   c - (c < 91 ? 38 : 96)
-}
+end
