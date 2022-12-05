11416fac3e9f32259bf5ba34101385cc7cf1ac7e
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-05 18:09:08 +0200

    shortening code

--- 2022/Day5/tasks.rb
+++ 2022/Day5/tasks.rb
@@ -1,32 +1,22 @@
-input = File.readlines(ARGV[0], chomp: true)
 task = ARGV[1].to_i # 1 or 2
-COUNT = (input[0].length + 1)/4
+stacks = []
+input = File.open(ARGV[0],"r")
 
-stacks = Array.new(COUNT) { [] }
-commands = []
-mode = "reading_crates"
-
-input.each do |line|
+while line = input.gets.chomp
   next if line.start_with?(" 1")
+  break if line.empty?
 
-  if mode == "reading_crates"
-    (0...COUNT).each do |ndx|
-      crate = line.chars[ndx * 4 + 1].to_s.strip
-      stacks[ndx] << crate if !crate.empty?
-    end
+  (1...line.size).step(4) do |ndx|
+    crate = line.chars[ndx].to_s.strip
+    stacks[ndx / 4] ||= []
+    stacks[ndx / 4] << crate if !crate.empty?
   end
-  
-  if mode == "reading_moves"
-    nums = line.scan(/\d+/).map(&:to_i)
-    commands << { count: nums[0], from: nums[1] - 1, to: nums[2] - 1}
-  end
-
-  mode = "reading_moves" if line.empty?
 end
 
-commands.each do |cmd|
-  taken = stacks[cmd[:from]].shift(cmd[:count])
-  stacks[cmd[:to]].unshift(task == 1 ? taken.reverse : taken).flatten!
+while line = input.gets
+  nums = line.scan(/\d+/).map(&:to_i)
+  taken = stacks[nums[1] - 1].shift(nums[0])
+  stacks[nums[2] - 1].unshift(task == 1 ? taken.reverse : taken).flatten!
 end
 
 p "Code: #{ stacks.map(&:first).join }"
