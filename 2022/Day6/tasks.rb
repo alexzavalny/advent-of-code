bdca8b62bd030ac775ae9dbdb1b5c7b51b929726
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-06 13:00:55 +0200

    even shorter code

--- 2022/Day6/tasks.rb
+++ 2022/Day6/tasks.rb
@@ -2,5 +2,5 @@ str = File.readlines(ARGV[0]).first.chars
 MARKER = 4
 MSG = 14
 
-puts ((0..str.size).find { |i| str[i..i+MARKER-1].uniq.size==MARKER }) + MARKER
-puts ((0..str.size).find { |i| str[i..i+MSG-1].uniq.size==MSG }) + MSG
+puts (MARKER..str.size).find { |i| str[i-MARKER..i-1].uniq.size==MARKER }
+puts (MSG..str.size).find { |i| str[i-MSG..i-1].uniq.size==MSG }
