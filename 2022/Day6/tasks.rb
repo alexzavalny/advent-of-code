ebd9eb26dc5d95bbf043165b4275093664adaede
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-06 14:16:51 +0200

    even shorter one liners

--- 2022/Day6/tasks.rb
+++ 2022/Day6/tasks.rb
@@ -1,6 +1,4 @@
 str = File.read(ARGV[0]).chars
 
-search = -> (s, len) { (len..s.size).find { |i| s[i-len..i-1].uniq.size==len } }
-
-puts search.(str, 4)
-puts search.(str, 14)
+p str.each_cons(4).map { _1.uniq.size }.index(4) + 4
+p str.each_cons(14).map { _1.uniq.size }.index(14) + 14
