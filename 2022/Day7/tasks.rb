26674e25169f9605bafe8b7a9fd1339ddeea1128
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-07 15:25:28 +0200

    refinements for day 7

--- 2022/Day7/tasks.rb
+++ 2022/Day7/tasks.rb
@@ -15,19 +15,13 @@ def total_size(dir)
   dir[:size] + dir[:folders].sum { total_size(_1) }
 end
 
-def find_smaller(dir, max_size)
-  small_dirs = dir[:folders].select { total_size(_1) <= max_size }
-  [small_dirs + dir[:folders].map { find_smaller(_1, max_size) }].flatten
+def sizes(dir)
+  under = dir[:folders].map { sizes(_1) }
+  [total_size(dir), under].flatten
 end
 
-def find_bigger(dir, min_size)
-  big_dirs = dir[:folders].select { total_size(_1) >= min_size }
-  [big_dirs + dir[:folders].map { find_bigger(_1, min_size) }].flatten
-end
-
-#task 1 
-p find_smaller(root, 100000).sum { total_size(_1) }
+puts "Task 1: #{ sizes(root).select { _1 < 100000 }.sum }"
 
 # #task 2
 need_to_free = 30000000 - (70000000 - total_size(root))
-p find_bigger(root, need_to_free).map { total_size(_1) }.min
+puts "Task 2: #{ sizes(root).select { _1 >= need_to_free }.min }"
