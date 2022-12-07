25d63d27a3f7578c1c3522172027a641d3daa74d
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-07 18:31:25 +0200

    adding to size of all parents

--- 2022/Day7/tasks.rb
+++ 2022/Day7/tasks.rb
@@ -1,27 +1,20 @@
 root = { folders: [], size: 0 }
 path = [root]
-
+sizes = {}
 File.readlines(ARGV[0], chomp: true).each do |str|
   case str
     when /\$ cd \//, /\$ ls/
     when /dir/ then path.last[:folders] << { name: str.split()[1], size: 0, folders: [] }
     when /\$ cd \.\./ then path.pop
     when /\$ cd/ then path << path.last[:folders].find { _1[:name] == str.split()[2] }
-    when /./ then path.last[:size] += str.split()[0].to_i
+    when /./ then path.each { _1[:size] += str.split()[0].to_i }
   end
 end
 
-def total_size(dir)
-  dir[:size] + dir[:folders].sum { total_size(_1) }
+def flatten_sizes(dir)
+  [dir[:size], dir[:folders].map { flatten_sizes(_1) }].flatten
 end
 
-def sizes(dir)
-  under = dir[:folders].map { sizes(_1) }
-  [total_size(dir), under].flatten
-end
-
-puts "Task 1: #{ sizes(root).select { _1 < 100000 }.sum }"
-
-# #task 2
-need_to_free = 30000000 - (70000000 - total_size(root))
-puts "Task 2: #{ sizes(root).select { _1 >= need_to_free }.min }"
+sizes = flatten_sizes(root)
+puts "Task 1: #{ sizes.select { _1 < 10**5 }.sum }"
+puts "Task 2: #{ sizes.select { _1 >= sizes[0] - 40000000 }.min }"
