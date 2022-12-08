ee5165c55eb3dd5fdb5d284751e9c8ccf319b83d
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-08 13:53:08 +0200

    better naming

--- 2022/Day8/task_1.rb
+++ 2022/Day8/task_1.rb
@@ -3,17 +3,17 @@ forest = File.readlines(ARGV[0]).map { |x| x.chars.map { |y| y.to_i } }
 visible = Set[]
 MAX = forest.count
 
-trans = -> (x, y, dir) { return [[x, y], [x, MAX-y-1], [y, x], [MAX-y-1, x]][dir] }
+convert_direction = -> (x, y, dir) { return [[x, y], [x, MAX-y-1], [y, x], [MAX-y-1, x]][dir] }
 
 (0..3).each do |direction|
   (0...MAX).each do |i|
-    max_height = [-1] * 4
+    max_heights = [-1] * 4
 
     (0...MAX).each do |j|
-      x, y = trans.(i, j, direction)
+      x, y = convert_direction.(i, j, direction)
 
-      if forest[x][y] > max_height[direction]
-        max_height[direction] = forest[x][y]
+      if forest[x][y] > max_heights[direction]
+        max_heights[direction] = forest[x][y]
         visible.add "#{x}_#{y}"
       end
     end
