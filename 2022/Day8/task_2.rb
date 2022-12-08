cc5a1559dc6758c676f60d00ac43b67ba7afc7d3
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-08 13:37:06 +0200

    removing lambdas

--- 2022/Day8/task_2.rb
+++ 2022/Day8/task_2.rb
@@ -1,23 +1,18 @@
 forest = File.readlines(ARGV[0]).map { |x| x.chars.map { |y| y.to_i } }
 MAX = forest.count
 
-tran1 = -> (x, y) { return x + 1, y }
-tran2 = -> (x, y) { return x - 1, y}
-tran3 = -> (x, y) { return x, y + 1}
-tran4 = -> (x, y) { return x, y - 1}
-trans = [tran1, tran2, tran3, tran4]
-
+shift = [ [1, 0], [-1, 0], [0, 1], [0, -1] ]
 max_score = 0
 
 (0...MAX).each do |i|
   (0...MAX).each_with_object(1) do |j, score|
     (0..3).each_with_object(0) do |m, count|
-      x, y = trans[m].(i, j)
+      x, y = i + shift[m][0], j + shift[m][1]
 
       while x >= 0 && y >= 0 && x < MAX && y < MAX
         count += 1
         break if forest[i][j] <= forest[x][y]
-        x, y = trans[m].(x, y)
+        x, y = x + shift[m][0], y + shift[m][1]
       end
 
       score *= count
