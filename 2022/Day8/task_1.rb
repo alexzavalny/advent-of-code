86610d3f9e2ade4af02f3919a673f00fbac2e81e
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-08 13:40:17 +0200

    removing lambdas

--- 2022/Day8/task_1.rb
+++ 2022/Day8/task_1.rb
@@ -3,21 +3,17 @@ forest = File.readlines(ARGV[0]).map { |x| x.chars.map { |y| y.to_i } }
 visible = Set[]
 MAX = forest.count
 
-tran1 = -> (x, y) { return x, y }
-tran2 = -> (x, y) { return x, MAX-y-1 }
-tran3 = -> (x, y) { return y, x}
-tran4 = -> (x, y) { return MAX-y-1, x }
-trans = [tran1, tran2, tran3, tran4]
+trans = -> (x, y, dir) { return [[x, y], [x, MAX-y-1], [y, x], [MAX-y-1, x]][dir] }
 
 (0..3).each do |direction|
   (0...MAX).each do |i|
-    mx = [-1, -1, -1, -1]
+    max_height = [-1] * 4
 
     (0...MAX).each do |j|
-      x, y = trans[direction].(i, j)
+      x, y = trans.(i, j, direction)
 
-      if forest[x][y] > mx[direction]
-        mx[direction] = forest[x][y]
+      if forest[x][y] > max_height[direction]
+        max_height[direction] = forest[x][y]
         visible.add "#{x}_#{y}"
       end
     end
