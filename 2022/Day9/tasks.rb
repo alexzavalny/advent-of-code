db7497ea38a12f36e44e045c866f5fdcee98b77d
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-09 12:39:10 +0200

    refinements

--- 2022/Day9/tasks.rb
+++ 2022/Day9/tasks.rb
@@ -1,42 +1,24 @@
 require 'set'
 input = File.readlines(ARGV[0], chomp: true)
 LENGTH = ARGV[1].to_i
+rope = [[0, 0]] * LENGTH
 
 visited = Set[]
 move = { "R" => [1, 0], "L" => [-1, 0], "U" => [0, 1], "D" => [0, -1] }
-rope = [[0, 0]] * LENGTH
+abs_max = -> (x) { x < 0 ? [x, -1].max : [x, 1].min }
 
 input.each do |line|
-  dir = line.split(" ")[0]
-  step = line.split(" ")[1].to_i
+  dir, step = line.split
 
-  step.times do 
+  step.to_i.times do 
     rope[0] = [rope[0][0] + move[dir][0], rope[0][1] + move[dir][1]]
 
     (LENGTH-1).times do |i|
-      rope[i+1] = [rope[i+1][0], rope[i+1][1]]
+      rope[i+1] = rope[i+1].clone
 
-      if rope[i+1][0] == rope[i][0] && (rope[i+1][1] - rope[i][1]).abs == 2
-        rope[i+1][1] -= (rope[i+1][1] - rope[i][1])/2
-      end
-
-      if rope[i+1][1] == rope[i][1] && (rope[i+1][0] - rope[i][0]).abs == 2
-        rope[i+1][0] -= (rope[i+1][0] - rope[i][0])/2
-      end
-
-      if (rope[i+1][0] - rope[i][0]).abs == 2 && (rope[i+1][1] - rope[i][1]).abs == 1
-        rope[i+1][1] -= (rope[i+1][1] - rope[i][1])
-        rope[i+1][0] -= (rope[i+1][0] - rope[i][0]) / 2
-      end
-
-      if (rope[i+1][1] - rope[i][1]).abs == 2 && (rope[i+1][0] - rope[i][0]).abs == 1
-        rope[i+1][0] -= (rope[i+1][0] - rope[i][0])
-        rope[i+1][1] -= (rope[i+1][1] - rope[i][1]) / 2
-      end
-
-      if (rope[i+1][1] - rope[i][1]).abs == 2 && (rope[i+1][0] - rope[i][0]).abs == 2
-        rope[i+1][0] -= (rope[i+1][0] - rope[i][0]) / 2
-        rope[i+1][1] -= (rope[i+1][1] - rope[i][1]) / 2
+      if (rope[i+1][0] - rope[i][0]).abs >= 2 || (rope[i+1][1] - rope[i][1]).abs >= 2
+        rope[i+1][0] -= abs_max.(rope[i+1][0] - rope[i][0])
+        rope[i+1][1] -= abs_max.(rope[i+1][1] - rope[i][1])
       end
     end
 
