d7fe9efd34e2755558d3d53823536e2db29a57a5
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-09 17:36:32 +0200

    back to vectors

--- 2022/Day9/tasks.rb
+++ 2022/Day9/tasks.rb
@@ -1,20 +1,21 @@
 require "set"
+require "matrix"
 
 def solution(instuctions, length)
-  move = { "R" => [1, 0], "L" => [-1, 0], "U" => [0, 1], "D" => [0, -1] }
-  rope = [[0, 0]] * length
+  move = { "R" => Vector[1, 0], "L" => Vector[-1, 0], "U" =>Vector[0, 1], "D" => Vector[0, -1] }
+  rope = [Vector[0, 0]] * length
   visited = Set[]
 
   instuctions.each do |dir, step|
     step.to_i.times do 
-      rope[0] = [rope[0][0] + move[dir][0], rope[0][1] + move[dir][1]]
+      rope[0] += move[dir]
 
       (length-1).times do |i|
         cur, nex = rope[i], rope[i+1]
         need_to_move = (nex[0] - cur[0]).abs == 2 || (nex[1] - cur[1]).abs == 2
         
         if need_to_move
-          rope[i+1] = [nex[0] - (nex[0] <=> cur[0]), nex[1] - (nex[1] <=> cur[1])]
+          rope[i+1] += Vector[cur[0] <=> nex[0], cur[1] <=> nex[1]]
         end
       end
 
