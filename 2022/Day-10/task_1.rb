4e498fb34c3c79fbbd2b05bf57f77225b770f9ef
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-10 15:21:16 +0200

    ignoring commands all together

--- 2022/Day-10/task_1.rb
+++ 2022/Day-10/task_1.rb
@@ -1,14 +1,14 @@
 # The trick I use here is in the reduce function
 # Turns out if you replace prepend addx 0 before each addx, you can ignore cycles
 # and if you replace noop with add x, then you can ignore command names (all will be addx)
-# so in reduce I keep only values of addx (0 for noop)
+# so I keep only values of addx (0 for noop)
 
 def solution(input)
   x_register = 1
 
   input
-    .map(&:split)
-    .reduce([]) { |acc, ins| acc << 0; acc << ins[1] if ins[1]; acc }
+    .map { _1.split[1] }
+    .reduce([]) { |acc, ins| acc << 0; acc << ins if ins; acc }
     .map
     .with_index(1) do |value, i|
       signal = x_register * i
