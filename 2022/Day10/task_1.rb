a6abd24a7694489d09324308fddb1a0bedeee3cb
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-10 13:32:50 +0200

    Day 10

--- 2022/Day10/task_1.rb
+++ 2022/Day10/task_1.rb
@@ -1,40 +1,21 @@
 def solution(input)
-  cycle = 0
-  x = 1
-  signals = 0
+  cycle, signals = 0, 0
+  x_register = 1
 
-  good_cycle = -> (c) { (c - 20) % 40 == 0}
+  input.map(&:split).each do |command, value|
+    cycles_per_command = {"noop" => 1, "addx" => 2}
 
-  input.each do |inst|
-    if inst == "noop"
+    cycles_per_command[command].times do
       cycle += 1
-      signals += (x*cycle) if good_cycle.(cycle)
-      puts x if good_cycle.(cycle)
-      #puts "Cycle: #{cycle}, X: #{x}"
-    else
-      add_value = inst.split.last.to_i
-      #puts "addx #{add_value}"
-      cycle += 1
-      signals += (x*cycle) if good_cycle.(cycle)
-      
-      puts x if good_cycle.(cycle)
-      #puts "Cycle: #{cycle}, X: #{x}"
-      cycle += 1
-
-      puts x if good_cycle.(cycle)
-      signals += (x*cycle) if good_cycle.(cycle)
-      x += add_value
-      #puts "Cycle: #{cycle}, X: #{x}"
-      #puts x if good_cycle.(cycle)
-      
+      good_cycle = (cycle - 20) % 40 == 0
+      signals += (x_register*cycle) if good_cycle
     end
+  
+    x_register += value.to_i if command == "addx"
   end
 
-  puts "----"
   signals
 end
 
 puts solution(File.readlines("input0.txt", chomp: true))
-puts "13140"
-puts "-"
 puts solution(File.readlines("input1.txt", chomp: true))
--- 2022/Day10/task_2.rb
+++ 2022/Day10/task_2.rb
@@ -1,62 +1,24 @@
 def solution(input)
-  cycle = 0
-  x = 1
+  x_register = 1
   screen = Array.new(6) { Array.new(40) { '?' } }
-
-  cor_x = 0
-  cor_y = 0
+  cor_x, cor_y = 0, 0
   
-  input.each do |inst|
-    if inst == "noop"
-      cycle += 1
+  input.map(&:split).each do |command, value|
+    cycles_per_command = {"noop" => 1, "addx" => 2}
 
-      sign = (cor_x - x).abs < 2 ? "#" : "."
+    cycles_per_command[command].times do
+      close_to_cursor = (cor_x - x_register).abs < 2 
+      sign = close_to_cursor ? "#" : "."
       screen[cor_y][cor_x] = sign
 
-      cor_x += 1
-      if cor_x % 40 == 0
-        cor_y += 1
-        cor_x = 0
-      end
-
-
-    else
-      add_value = inst.split.last.to_i
-      cycle += 1
-      
-
-      
-
-      sign = (cor_x - x).abs < 2 ? "#" : "."
-      screen[cor_y][cor_x] = sign
-      cor_x += 1
-      if cor_x % 40 == 0
-        cor_y += 1
-        cor_x = 0
-      end
-
-
-      cycle += 1
-
-
-      
-
-      sign = (cor_x - x).abs < 2 ? "#" : "."
-      screen[cor_y][cor_x] = sign
-      cor_x += 1
-      if cor_x % 40 == 0
-        cor_y += 1
-        cor_x = 0
-      end
-
-      x += add_value
+      cor_x = (cor_x + 1) % 40
+      cor_y += 1 if cor_x.zero?
     end
+
+    x_register += value.to_i if command == "addx"
   end
 
-  puts screen.map(&:join)
+  screen.map(&:join)
 end
 
-solution(File.readlines("input1.txt", chomp: true))
-#puts "13140"
-#puts "-"
-#puts solution(File.readlines("input1.txt", chomp: true))
+puts solution(File.readlines("input1.txt", chomp: true))
