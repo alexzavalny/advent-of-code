748dc207e55fde2d9ed6d447f881b9b5ef3e5090
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-10 16:10:01 +0200

    reduce magic for day 10

--- 2022/Day-10/task_2.rb
+++ 2022/Day-10/task_2.rb
@@ -1,22 +1,17 @@
 def solution(input)
   x_register = 1
-  screen = Array.new(6) { Array.new(40) { '?' } }
-  cor_x, cor_y = 0, 0
+  screen = Array.new(6) { Array.new(40) { "" } }
   
-  input.map(&:split).each do |command, value|
-    cycles_per_command = {"noop" => 1, "addx" => 2}
-
-    cycles_per_command[command].times do
-      close_to_cursor = (cor_x - x_register).abs < 2 
+  input
+    .map { _1.split[1] }
+    .reduce([]) { |acc, ins| acc << 0; acc << ins if ins; acc }
+    .map
+    .with_index(0) do |value, i|
+      close_to_cursor = (i % 40 - x_register).abs <= 1 
       sign = close_to_cursor ? "#" : "."
-      screen[cor_y][cor_x] = sign
-
-      cor_x = (cor_x + 1) % 40
-      cor_y += 1 if cor_x.zero?
+      screen[i / 40][i % 40] = sign
+      x_register += value.to_i 
     end
-
-    x_register += value.to_i if command == "addx"
-  end
 
   screen.map(&:join)
 end
