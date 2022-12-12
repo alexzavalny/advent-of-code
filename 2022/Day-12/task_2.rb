06a2d4aef125b955c3ee9a540c1a4e9e88a77bc7
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-12 17:18:42 +0200

    going from back to start

--- 2022/Day-12/task_2.rb
+++ 2022/Day-12/task_2.rb
@@ -1,16 +1,16 @@
 require 'set'
 
 def solution(input)
-  end_pos = []
-
-  starting_points = []
+  end_pos = Set[]
+  starting_pos = []
+  
   hmap = input.map.with_index do | line, y |
     line.chars.map.with_index do |char, x|
       if char == "S" || char == "a"
-        starting_points << [x, y]
+        end_pos << [x, y]
         1
       elsif char == "E"
-        end_pos = [x, y]
+        starting_pos = [x, y]
         26
       else
         char.ord - 'a'.ord + 1
@@ -19,22 +19,23 @@ def solution(input)
   end
 
   width, height = hmap[0].size, hmap.size
-  queue = starting_points.map { { step: 0, position: _1 } }
+  queue = [ { step: 0, position: starting_pos } ]
 
   visited = Set[]
+  
   while queue.size > 0
     elem = queue.slice!(0)
     next if visited.include?(elem[:position])
-    return elem[:step] if elem[:position] == end_pos
+    return elem[:step] if end_pos.include?(elem[:position])
 
     visited << elem[:position]
     x, y = elem[:position]
     step = elem[:step]
 
-    queue << { step: step + 1, position: [x, y-1] } if y - 1 >= 0 && hmap[y - 1][x] - hmap[y][x]<= 1 unless visited.include?([x, y-1])
-    queue << { step: step + 1, position: [x, y+1] } if y + 1 < height && hmap[y + 1][x] - hmap[y][x]<= 1 unless visited.include?([x, y+1])
-    queue << { step: step + 1, position: [x-1, y] } if x - 1 >= 0 && hmap[y][x - 1] - hmap[y][x]<= 1 unless visited.include?([x-1, y])
-    queue << { step: step + 1, position: [x+1, y] } if x + 1 < width && hmap[y][x + 1] - hmap[y][x] <= 1 unless visited.include?([x+1, y])
+    queue << { step: step + 1, position: [x, y-1] } if y - 1 >= 0 && hmap[y][x] - hmap[y - 1][x]  <= 1 unless visited.include?([x, y-1])
+    queue << { step: step + 1, position: [x, y+1] } if y + 1 < height && hmap[y][x] - hmap[y + 1][x] <= 1 unless visited.include?([x, y+1])
+    queue << { step: step + 1, position: [x-1, y] } if x - 1 >= 0 && hmap[y][x] - hmap[y][x - 1] <= 1 unless visited.include?([x-1, y])
+    queue << { step: step + 1, position: [x+1, y] } if x + 1 < width && hmap[y][x] - hmap[y][x + 1]  <= 1 unless visited.include?([x+1, y])
   end
 end
 
