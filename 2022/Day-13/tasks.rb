3048003238618ba9dccd3d5fbec2bd4a1978bf1a
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-13 13:54:30 +0200

    style improvements

--- 2022/Day-13/tasks.rb
+++ 2022/Day-13/tasks.rb
@@ -1,8 +1,7 @@
 def right_order?(l, r) 
-  return r <=> l if [l, r].all? { _1.is_a?(Integer) }
+  return l <=> r if [l, r].all? { _1.is_a?(Integer) }
 
-  l = [l] if l.is_a?(Integer) && r.is_a?(Array)
-  r = [r] if l.is_a?(Array) && r.is_a?(Integer)
+  l, r = Array(l), Array(r)
 
   l.each_with_index do |_, index|
     break if r.size <= index
@@ -10,7 +9,7 @@ def right_order?(l, r)
     return r_o unless r_o.zero?
   end
 
-  return r.size <=> l.size
+  return l.size <=> r.size
 end
 
 def solution1(input)
@@ -26,7 +25,7 @@ end
 def solution2(input)
   packets = input.split("\n").filter { |p| p != "" }.map { |packet| eval(packet) }
   packets << [[2]] << [[6]]
-  sorted = packets.sort { |a, b| -right_order?(a, b) }
+  sorted = packets.sort { |a, b| right_order?(a, b) }
   return (sorted.index([[2]]) + 1) * (sorted.index([[6]]) + 1)
 end
 
