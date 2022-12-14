04c7830d8c54622c99ac4dca45a0b6825ffab7d7
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-14 10:56:02 +0200

    day 14

---
+++ 2022/Day-14/task_2.rb
@@ -0,0 +1,73 @@
+require 'set'
+
+def solution(input)
+  rocks = Set[]
+  lowest_point = 0
+
+  input.each do |line|
+    blocks = line.split(" -> ")
+    
+    (blocks.size - 1).times do |ndx|
+      block1 = blocks[ndx].split(",").map(&:to_i)
+      block2 = blocks[ndx + 1].split(",").map(&:to_i)
+
+      lowest_point = [block1[1], block2[1], lowest_point].max
+
+      if block1[0] == block2[0]
+        ([block1[1], block2[1]].min..[block1[1], block2[1]].max).each do |y|
+          rocks << [block1[0], y]
+        end
+      end
+
+      if block1[1] == block2[1]
+        ([block1[0], block2[0]].min..[block1[0], block2[0]].max).each do |x|
+          rocks << [x, block1[1]]
+        end
+      end
+    end
+  end
+
+  sands = 0
+
+  while !rocks.include?([500, 0]) do
+    sand = [500, 0]
+    sands += 1
+    moving = true
+
+    while moving
+      if sand[1] > lowest_point
+        break
+      end
+
+      if sand[1] + 1 > lowest_point + 2
+        moving = false
+        break
+      end
+
+      if !rocks.include?([sand[0], sand[1] + 1])
+        sand[1] += 1
+        next
+      end
+
+      if !rocks.include?([sand[0] - 1, sand[1] + 1])
+        sand[1] += 1
+        sand[0] -= 1
+        next
+      end
+
+      if !rocks.include?([sand[0] + 1, sand[1] + 1])
+        sand[1] += 1
+        sand[0] += 1
+        next
+      end
+
+      moving = false
+    end
+
+    rocks << sand
+  end
+
+  sands
+end
+
+puts solution(File.readlines('input1.txt', chomp: true))
