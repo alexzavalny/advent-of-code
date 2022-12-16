7e3aff8c31f8e11a924b936df7a75cd643c56741
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-16 17:37:55 +0200

    version

--- 2022/Day-16/task_2.rb
+++ 2022/Day-16/task_2.rb
@@ -15,7 +15,7 @@ module Day16
     def path_to_str(path)
       path.map do |step|
         if step[:action] == :open
-          "[open #{step[:place]}]"
+          "[open #{step[:place]} (#{ @valves[step[:place]][:rate] })]"
         else
           "[move #{step[:place]} -> #{step[:destination]}]"
         end
@@ -28,7 +28,7 @@ module Day16
 
       rez = path.last[:flow]
       if rez > @best
-        #puts "Scoring #{ path_to_str(path) }"
+        puts "Scoring #{ path_to_str(path) }"
         puts "New best: #{rez}"
         @best = rez
       end
@@ -37,15 +37,6 @@ module Day16
     end
 
     def should_skip?(path, way)
-      # @max_opened[path.size] ||= 0
-      return false if path.size < 5
-      # if @max_opened[path.size] > path.last[:flow] && path.size > 10
-      #   return false
-      # end
-      # @max_opened[path.size] = path.last[:flow]
-
-      
-
       i = path.size - 1
       while i >= 0
         return false if path[i][:action] == :open
@@ -72,27 +63,24 @@ module Day16
     def best_way(opened, cur_valve, need_open, path)
       return 0 if should_kill(path)
       return score(path) if path.size == MINUTES_TOTAL #we did all we can
-      
+
       if need_open
         last_flow = path.size > 0 ? path.last[:flow] : 0
-        path << {place: cur_valve, action: :open, opened: opened.clone, flow: last_flow + calculate_flow(opened) } 
-        opened = opened.clone
-        opened << cur_valve
-        return best_way(opened, cur_valve, false, path)
+        elem = {place: cur_valve, action: :open, opened: opened.clone, flow: last_flow + calculate_flow(opened) } 
+        return best_way(opened + [cur_valve], cur_valve, false, path + [elem])
       end
 
       leads = @valves[cur_valve][:leads]
       leads.each do |way|
         next if should_skip?(path, way)
-        path2 = path.dup
         last_flow = path.size > 0 ? path.last[:flow] : 0
-        path2 << {place: cur_valve, action: :move, destination: way, opened: opened.clone, flow: last_flow + calculate_flow(opened)}
+        elem = {place: cur_valve, action: :move, destination: way, opened: opened.clone, flow: last_flow + calculate_flow(opened)}
 
         if !opened.include?(way) && @valves[way][:rate] > 0
-          best_way(opened.clone, way, true, path2.dup)
+          best_way(opened, way, true, path + [elem])
         end
 
-        best_way(opened.clone, way, false, path2.dup)
+        best_way(opened, way, false, path + [elem])
       end
     end
 
@@ -100,6 +88,7 @@ module Day16
       keys = []
 
       ndx = 1
+
       input.each do |line|
         rate = line.scan(/\d+/).first.to_i
         all_valves = line.scan(/[A-Z][A-Z]/).map(&:to_sym)
@@ -112,9 +101,11 @@ module Day16
 
       @sum_rates = keys.sum { @valves[_1][:rate] }
 
-      best_way(Set[], :AA, false, [])
+      best_way([], :AA, false, [])
+      ""
     end
   end
+  
 end
 
 puts Day16::Solution.new.run(File.readlines("input1.txt"))
