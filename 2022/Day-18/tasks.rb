0c567e97676667778c82afe57d32274f50af3c56
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-18 20:49:36 +0200

    removed puts

--- 2022/Day-18/tasks.rb
+++ 2022/Day-18/tasks.rb
@@ -1,15 +1,5 @@
 cubes = File.readlines("input1.txt").map { |l| l.split(",").map(&:to_i) }
 
-puts "min"
-
-puts cubes.min { |a, b| a.min <=> b.min }.min
-
-puts "max"
-
-puts cubes.max { |a, b| a.max <=> b.max }.max
-
-puts "---"
-
 def are_siblings?(cube1, cube2)
   (cube1[0]-cube2[0]).abs + (cube1[1]-cube2[1]).abs + (cube1[2]-cube2[2]).abs == 1
 end
@@ -29,8 +19,8 @@ def calculate_surface(cubes)
 end
 
 sum = calculate_surface(cubes)
-p "Size of cubes: #{cubes.size}"
-p sum
+
+p "Answer 1", sum
 
 aircubes = []
 
@@ -42,22 +32,10 @@ aircubes = []
   end
 end
 
-p aircubes.size
-
 aircubes = aircubes - cubes
 
-puts "Substracting lava cubes"
-
-p aircubes.size
-
-p aircubes[0..10]
-
-puts "Removing all 1, 1, 1 siblings"
-
 que = [ [1, 1, 1 ]]
-
 while que.size > 0
-  #puts "Que size = #{que.size}. Aircubes size = #{aircubes.size}"
   i = aircubes.size - 1
   current = que.shift
 
@@ -71,12 +49,7 @@ while que.size > 0
   end
 end
 
-puts "done..."
-
-p aircubes
-
 inner_air = calculate_surface(aircubes)
 
-puts "Inner air surface = #{inner_air}"
-
-puts "Normal surface = #{sum - inner_air}"
+puts "Answer 2" 
+puts "#{sum - inner_air}"
