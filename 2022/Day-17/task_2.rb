4f1fe1b9fa23659a524065a487d43de6ae1aee6e
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-19 09:40:08 +0200

    pattern print

---
+++ 2022/Day-17/task_2.rb
@@ -0,0 +1,148 @@
+def print_chamber_with_rock
+  #return
+  puts "[#{@step}] Chamber height #{@chamber.size}. Rock coords: #{@xpos}, #{@ypos}:"
+  return
+  x = 0
+  (@ypos).times do |my|
+    x = x + 1
+    return if x == 20
+    y = @ypos - my
+    print '|'
+    (7).times do |x|
+      rockx = x - @xpos
+      rocky = y - @ypos + rock_height - 1
+
+      if !@current_rock.nil? && rockx >= 0 && rocky >= 0 && rocky < @current_rock.size && 
+        rockx < @current_rock[rocky].size && @current_rock[rocky][rockx] == 1
+        print '@'
+      elsif y < @chamber.size && x < @width && @chamber[y][x] == 1
+        print "#"
+      else
+        print '.'
+      end
+    end
+    print '|'
+    print("\n")
+  end
+
+  print "+-------+\n"
+  puts
+  #gets
+end
+
+def would_overlap?(new_x, new_y)
+  @current_rock.size.times do |rock_y|
+    inv_y = @current_rock.size - rock_y - 1
+    @current_rock[0].size.times do |rock_x|
+      if @current_rock[inv_y][rock_x] == 1
+        if !@chamber[new_y - rock_y].nil? && @chamber[new_y - rock_y][rock_x + new_x] == 1
+          return true
+        end
+      end
+    end
+  end
+
+  return false
+end
+
+def can_move_down?
+  #puts "checking if stuck #{@ypos} #{rock_height}"
+  return false if @ypos - rock_height == 0
+  return !would_overlap?(@xpos, @ypos - 1)
+end
+
+def can_move_left?
+  return false if @xpos == 0
+  return !would_overlap?(@xpos - 1, @ypos)
+end
+
+def can_move_right?
+  return false if @xpos + @current_rock.first.size == @width
+  return !would_overlap?(@xpos + 1, @ypos)
+end
+
+def rock_height
+  !@current_rock.nil? ? @current_rock.size : 0
+end
+
+def persist_rock
+  @chamber << [ 0, 0, 0, 0, 0, 0, 0, 0 ] while @chamber.size <= @ypos
+
+  @current_rock.size.times do |rock_y|
+    inv_y = @current_rock.size - rock_y - 1
+    @current_rock[0].size.times do |rock_x|
+      @chamber[@ypos - rock_y][rock_x + @xpos] = 1 if @current_rock[inv_y][rock_x] == 1
+    end
+  end
+end
+
+def solution(input)
+  @width = 7
+  @chamber = [[1] * 7] * 1
+  @xpos = 0
+  @ypos = 0
+  @increases = []
+
+  @rocks = [ 
+    [[1, 1, 1, 1]],
+    [[0, 1, 0], [1, 1, 1], [0, 1, 0]],
+    [[1, 1, 1], [0, 0, 1], [0, 0, 1]],
+    [[1], [1], [1], [1]],
+    [[1, 1],[1, 1]]
+  ]
+
+  @rock_index = 0
+  @current_rock = nil
+
+  @step = 0
+  direction_index = 0
+  chamber_size = 0
+  10000.times do
+    #start rock
+    
+    @current_rock = @rocks[@rock_index]
+    @xpos = 2
+    @ypos = @chamber.size + @current_rock.size + 3 - 1
+
+    #print_chamber_with_rock
+    #blow wind
+    while true
+      direction = input.chars[direction_index]
+      direction_index = (direction_index + 1) % input.chars.size
+      @step = @step + 1
+      #make movement if you can
+      if direction == "<"
+        #puts "moving left?"
+        if can_move_left?
+          @xpos -= 1
+          #print_chamber_with_rock
+        end
+      elsif direction == ">"
+        if can_move_right?
+          @xpos += 1
+          #print_chamber_with_rock
+        end
+      end
+
+      #if it is stuck, break
+      break if !can_move_down?
+
+      #move down
+      @ypos -= 1
+     #print_chamber_with_rock
+    end
+    puts "Chamber size = #{@chamber.size}. Previous chamber size = #{chamber_size}"
+    @increases << @chamber.size - chamber_size - 1
+    chamber_size = @chamber.size - 1
+    @rock_index = (@rock_index + 1) % @rocks.size
+
+    persist_rock    
+  end
+
+  @current_rock = nil
+  print_chamber_with_rock
+  File.open("pattern0.txt", "w") { |f| f.write @increases.join(",") }
+end
+
+require 'benchmark'
+puts Benchmark.measure { solution(File.readlines("input0.txt").first) }
