9cf29ab21dd8f62e43e9f506c89bdc854c4f17b2
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-19 18:31:26 +0200

    few optimization

--- 2022/Day-19/tasks.rb
+++ 2022/Day-19/tasks.rb
@@ -6,29 +6,32 @@ def process(time_total, ore_robot_ore_price, cla_robot_ore_price, obs_robot_ore_
   state = [0, 0, 0, 0, 1, 0, 0, 0, time_total] #only have 1 ore robot 
   states = [state]
   visited = Set[]
+  max_ore_price = [ore_robot_ore_price, cla_robot_ore_price, obs_robot_ore_price, geo_robot_ore_price].max
 
   while states.size > 0
     state = states.shift
     ore, cla, obs, geo, ore_robots, cla_robots, obs_robots, geo_robots, time_left = state
-  
-    max_geo = [max_geo, geo].max
 
-    next if time_left == 0
+    # NEXT LINE STOLEN FROM REDDIT AFTER GOLD STAR. But it works only for input, not for test. :-)
+    next if geo + geo_robots + time_left < max_geo
+    max_geo = [max_geo, geo + geo_robots].max
+
+    next if time_left == 1
     new_time_left = time_left - 1
 
     #magical optimization, cutting down ore money if we are unable to spend it, which will reduce state space
-    max_ore_price = [ore_robot_ore_price, cla_robot_ore_price, obs_robot_ore_price, geo_robot_ore_price].max
+    
     ore = [ore, time_left * max_ore_price - ore_robots * new_time_left].min
     #todo: same for all other currencies.
 
-    state = [ore, cla, obs, geo, ore_robots, cla_robots, obs_robots, geo_robots, time_left]
+    state = [ore, cla, obs, geo, ore_robots, cla_robots, obs_robots, geo_robots]
 
     next if visited.include?(state)
     visited << state
 
     # if you can buy geo robot, then do it and don't add any other chances
     if ore >= geo_robot_ore_price and obs >= geo_robot_cla_price # buy geo unit
-      states << [
+      states.prepend([ # PREPEND NEXT STOLEN FROM REDDIT AFTER GOLD STAR
         ore + ore_robots - geo_robot_ore_price, 
         cla + cla_robots, 
         obs + obs_robots - geo_robot_cla_price, 
@@ -37,7 +40,7 @@ def process(time_total, ore_robot_ore_price, cla_robot_ore_price, obs_robot_ore_
         obs_robots, 
         geo_robots + 1, 
         new_time_left
-      ]
+      ])
 
       next
     end
@@ -116,14 +119,16 @@ def solution1(input)
 end
 
 def solution2(input)
-  input[0..2].reduce([]) do |geods, line|
+  geods = []
+  input[0..2].each do |line|
     numbers = line.scan(/\d+/).map(&:to_i)
     blueprint_id, ore_cost, cla_cost, obs_robot_ore_price, obs_robot_cla_price, geo_robot_ore_price, geo_robot_cla_price = numbers
     puts "Processing #{blueprint_id}"
     rez = process(32, ore_cost, cla_cost, obs_robot_ore_price, obs_robot_cla_price, geo_robot_ore_price, geo_robot_cla_price)
     geods << rez
   end
-  .reduce(:*)
+  p geods
+  geods.reduce(:*)
 end
 
-puts Benchmark.measure { puts solution1(File.readlines("input0.txt")) }
+puts Benchmark.measure { puts solution1(File.readlines("input1.txt")) }
