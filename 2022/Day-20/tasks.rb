2428a5ff4fb2a187e66116d396570c8a681fd77d
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-20 13:42:38 +0200

    clean up

--- 2022/Day-20/tasks.rb
+++ 2022/Day-20/tasks.rb
@@ -3,16 +3,12 @@ def solution(numbers, coef = 1, cycles = 1)
   elements = numbers.map.with_index { |el, ndx| { ndx: ndx, val: el.to_i * coef } }
 
   cycles.times do
-    # we go through all numbers
     numbers.size.times do |ndx|
-      # find element
       before_index = elements.find_index { _1[:ndx] == ndx }
       element = elements[before_index]
 
-      # remove element from the list
       elements.delete_at(before_index)
 
-      # reinsert at new index
       after_index = (before_index + element[:val]) % elements.size
       elements.insert(after_index, element)
     end
@@ -21,7 +17,8 @@ def solution(numbers, coef = 1, cycles = 1)
   zero_pos = elements.find_index { _1[:val] == 0 }
 
   [1000, 2000, 3000].sum do |position| 
-    elements[(zero_pos + position) % numbers.size][:val]
+    circle_position = (zero_pos + position) % numbers.size
+    elements[circle_position][:val]
   end
 end
 
