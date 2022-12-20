ce7c5466c58ec341254bd598e78494509337bf9f
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-20 12:26:48 +0200

    optimized

---
+++ 2022/Day-20/tasks.rb
@@ -0,0 +1,32 @@
+def solution(coef, cycles)
+  numbers = File.readlines("input1.txt").map(&:to_i)
+  elements = []
+  numbers.each_with_index { |el, ndx| elements << { ndx: ndx, value: el.to_i * coef } }
+
+  count = numbers.size
+  cycles.times do
+    count.times do |ndx|
+      found_ndx = elements.find_index { _1[:ndx] == ndx }
+      el = elements[found_ndx] 
+
+      elements.delete_at(found_ndx)
+      new_ndx = found_ndx + el[:value]
+        new_ndx %= (count - 1)
+
+      # for some reason, element gets to the end, not to the start
+      new_ndx = count - 1 if new_ndx == 0
+
+      elements.insert(new_ndx, el)
+    end
+  end
+
+  zero_pos = elements.find_index { _1[:value] == 0 }
+  p_1000 = zero_pos + 1000
+  p_2000 = zero_pos + 2000
+  p_3000 = zero_pos + 3000
+
+  elements[p_1000 % count][:value] + elements[p_2000 % count][:value] + elements[p_3000 % count][:value]
+end
+
+puts "Part1: #{ solution(1, 1) }"
+puts "Part2: #{ solution(811589153, 10) }"
