8e72b8c37adb4f1a99b752583028f8b65cb02760
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-20 09:08:56 +0200

    cleaning up

--- 2022/Day-20/task_2.rb
+++ 2022/Day-20/task_2.rb
@@ -1,58 +1,33 @@
 numbers = File.readlines("input1.txt").map(&:to_i)
-
 elements = []
-
-numbers.each_with_index do |el, ndx|
-  elements << { ndx: ndx, value: el.to_i * 811589153}
-end
-
-def print(shit)
-  p shit.map { _1[:value] }
-end
-
-#print(elements)
+numbers.each_with_index { |el, ndx| elements << { ndx: ndx, value: el.to_i * 811589153 } }
 
 count = numbers.size
 
 10.times do |round|
   count.times do |ndx|
-    #find element
-    found_ndx = elements.find_index { |x| x[:ndx] == ndx }
+    found_ndx = elements.find_index { _1[:ndx] == ndx }
     el = elements[found_ndx] 
 
     elements.delete_at(found_ndx)
+
     new_ndx = found_ndx + el[:value]
-    while new_ndx < 0
-      #new_ndx += count - 1
+    while new_ndx < 0 || new_ndx >= count
       new_ndx %= (count - 1)
     end
 
-    while new_ndx >= count
-      new_ndx %= (count - 1) #new_ndx - count + 1
-    end
+    # for some reason, element gets to the end, not to the start
     new_ndx = count - 1 if new_ndx == 0
-    #puts "Will move #{el[:value]} from ndx #{found_ndx} to ndx #{new_ndx}"
 
     elements.insert(new_ndx, el)
-
-
   end
-
-  #puts "After #{round} round of mixing"
-#  print(elements)
-  #gets
 end
 
-zero_pos = elements.find_index { |x| x[:value] == 0 }
-p zero_pos
+zero_pos = elements.find_index { _1[:value] == 0 }
 p_1000 = zero_pos + 1000
 p_2000 = zero_pos + 2000
 p_3000 = zero_pos + 3000
-puts elements[p_1000 % count]
-puts elements[p_2000 % count]
-puts elements[p_3000 % count]
 
-puts elements[p_1000 % count][:value] + elements[p_2000 % count][:value] + elements[p_3000 % count][:value]
-#p numbers.size
-#p numbers.uniq.size
-#p numbers
+puts elements[p_1000 % count][:value] + 
+     elements[p_2000 % count][:value] + 
+     elements[p_3000 % count][:value]
