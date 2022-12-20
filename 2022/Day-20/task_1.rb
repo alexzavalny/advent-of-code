cf51f797cd20349017f923acf00de38d03f0b302
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-20 08:57:57 +0200

    Day 20 (dirty)

---
+++ 2022/Day-20/task_1.rb
@@ -0,0 +1,51 @@
+numbers = File.readlines("input1.txt").map(&:to_i)
+
+elements = []
+
+numbers.each_with_index do |el, ndx|
+  elements << { ndx: ndx, value: el.to_i }
+end
+
+def print(shit)
+  p shit.map { _1[:value] }
+end
+
+print(elements)
+
+count = numbers.size
+count.times do |ndx|
+  #find element
+  found_ndx = elements.find_index { |x| x[:ndx] == ndx }
+  el = elements[found_ndx] 
+
+  elements.delete_at(found_ndx)
+  new_ndx = found_ndx + el[:value]
+  while new_ndx < 0
+    new_ndx += count - 1# (new_ndx + count) % count - 1
+  end
+
+  while new_ndx >= count
+    new_ndx = new_ndx - count + 1
+  end
+  new_ndx = count - 1 if new_ndx == 0
+  #puts "Will move #{el[:value]} from ndx #{found_ndx} to ndx #{new_ndx}"
+
+  elements.insert(new_ndx, el)
+
+  #print(elements)
+  #gets
+end
+
+zero_pos = elements.find_index { |x| x[:value] == 0 }
+p zero_pos
+p_1000 = zero_pos + 1000
+p_2000 = zero_pos + 2000
+p_3000 = zero_pos + 3000
+puts elements[p_1000 % count]
+puts elements[p_2000 % count]
+puts elements[p_3000 % count]
+
+puts elements[p_1000 % count][:value] + elements[p_2000 % count][:value] + elements[p_3000 % count][:value]
+#p numbers.size
+#p numbers.uniq.size
+#p numbers
