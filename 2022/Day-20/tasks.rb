6de2d28476573574f32828f1287426f01f275b85
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-20 14:30:06 +0200

    renamings

--- 2022/Day-20/tasks.rb
+++ 2022/Day-20/tasks.rb
@@ -1,21 +1,20 @@
-def solution(numbers, coef = 1, cycles = 1)
+def solution(nums, coef = 1, cycles = 1)
   # [{ndx:0, val: 1}, {ndx: 1, val: 5},...]
-  elements = numbers.map.with_index { |el, ndx| { ndx: ndx, val: el.to_i * coef } }
+  items = nums.map.with_index { |el, ndx| { ndx: ndx, val: el.to_i * coef } }
 
   cycles.times do
-    numbers.size.times do |ndx|
-      before_index = elements.find_index { |el| el[:ndx] == ndx }
-      element = elements.delete_at(before_index)
-      after_index = (before_index + element[:val]) % elements.size
-      elements.insert(after_index, element)
+    nums.size.times do |ndx|
+      before_ndx = items.find_index { |el| el[:ndx] == ndx }
+      item = items.delete_at(before_ndx)
+      after_ndx = (before_ndx + item[:val]) % items.size
+      items.insert(after_ndx, item)
     end
   end
 
-  zero_pos = elements.find_index { |el| el[:val] == 0 }
+  i = items.find_index { |el| el[:val] == 0 }
 
-  [1000, 2000, 3000].sum do |position| 
-    circle_position = (zero_pos + position) % numbers.size
-    elements[circle_position][:val]
+  [1000, 2000, 3000].sum do |offset| 
+    items[(i + offset) % nums.size][:val]
   end
 end
 
