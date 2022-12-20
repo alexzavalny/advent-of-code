65ae7926b11100bd4bc543699b92e07ccdbbe20c
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-20 13:37:03 +0200

    more beautiful

--- 2022/Day-20/tasks.rb
+++ 2022/Day-20/tasks.rb
@@ -1,13 +1,10 @@
-def solution(filename, coef = 1, cycles = 1)
-  numbers = File.readlines(filename).map(&:to_i)
-  count = numbers.size
-
+def solution(numbers, coef = 1, cycles = 1)
   # storing not only numbers but initial indexes
   elements = numbers.map.with_index { |el, ndx| { ndx: ndx, val: el.to_i * coef } }
 
   cycles.times do
     # we go through all numbers
-    count.times do |ndx|
+    numbers.size.times do |ndx|
       # find element
       before_index = elements.find_index { _1[:ndx] == ndx }
       element = elements[before_index]
@@ -16,7 +13,7 @@ def solution(filename, coef = 1, cycles = 1)
       elements.delete_at(before_index)
 
       # reinsert at new index
-      after_index = (before_index + element[:val]) % (count - 1)
+      after_index = (before_index + element[:val]) % elements.size
       elements.insert(after_index, element)
     end
   end
@@ -24,13 +21,14 @@ def solution(filename, coef = 1, cycles = 1)
   zero_pos = elements.find_index { _1[:val] == 0 }
 
   [1000, 2000, 3000].sum do |position| 
-    elements[(zero_pos + position) % count][:val]
+    elements[(zero_pos + position) % numbers.size][:val]
   end
 end
 
 require 'benchmark'
 
 puts Benchmark.measure {
-  puts "Part1: #{ solution("input1.txt") }"
-  puts "Part2: #{ solution("input1.txt", 811589153, 10) }"
+  numbers = File.readlines("input1.txt").map(&:to_i)
+  puts "Part1: #{ solution(numbers) }"
+  puts "Part2: #{ solution(numbers, 811589153, 10) }"
 }
