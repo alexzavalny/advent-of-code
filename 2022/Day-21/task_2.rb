c6f4a0f07fa194c0c8f364c6710c3bac195738f8
Alex Zavalny <alexandr.zavalny@workday.com>
2022-12-21 14:06:49 +0200

    better

--- 2022/Day-21/task_2.rb
+++ 2022/Day-21/task_2.rb
@@ -1,6 +1,6 @@
 @monkeys = {}
 
-File.readlines("input0.txt", chomp: true).map do |l|
+File.readlines("input1.txt", chomp: true).map do |l|
   monkey = {}
   parts = l.split(":")
   eq = parts[1][1..-1]
@@ -8,7 +8,7 @@ File.readlines("input0.txt", chomp: true).map do |l|
     monkey = { value: eq.to_i }
   else
     eq_parts = eq.split(' ')
-    eq_parts[1] = "=" if eq_parts[0] == "root"
+    eq_parts[1] = "=" if parts[0] == "root"
     monkey = { left: eq_parts[0], op: eq_parts[1], right: eq_parts[2] }
   end
   @monkeys[parts[0]] = monkey
@@ -31,23 +31,17 @@ def build_equation(name)
   end
 end
 
-def print_eq(eq, level = 0)
+def print_eq(eq)
   return eq if eq.class.to_s == "Integer"
   return eq if eq == "x"
-  new_level = 0
-  new_level = 1 if eq[:op] == "="
-  if level == 1
-    return "( #{print_eq(eq[:left], new_level)} ___#{eq[:op]}___ #{print_eq(eq[:right], new_level)} )"
-  else
-    return "( #{print_eq(eq[:left], new_level)} #{eq[:op]} #{print_eq(eq[:right], new_level)} )"
-  end
+  return "( #{print_eq(eq[:left])} #{eq[:op]} #{print_eq(eq[:right])} )"
 end
 
 eq = build_equation("root")
 
 while (eq[:left] != "x")
-  p print_eq(eq)
-  puts
+  #puts print_eq(eq)
+  #gets
 
   if eq[:left][:op] == "+" 
     v = 0
@@ -88,6 +82,7 @@ while (eq[:left] != "x")
       eq[:left] = eq[:left][:left]
     end
     eq[:right] = eq[:right] / v
+
     next
   end
 
